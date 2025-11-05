190f4168a5634d0fb498da6af46dc821ebec8ed5
mrNo80dy <chad.mitchell.evans@gmail.com>
2025-11-04 22:52:08 -0600

    Update use-webrtc-hook.ts

--- hooks/use-webrtc-hook.ts
+++ hooks/use-webrtc-hook.ts
@@ -1,5 +1,6 @@
 "use client";
 
+
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { createSignalChannel } from "@/lib/realtime";
 
@@ -217,3 +218,4 @@ export function useWebRTC(
     toggleVideo,
   };
 }
+
