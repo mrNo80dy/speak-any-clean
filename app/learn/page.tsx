ccab353480456ad7098c6628fd487f619ed69e6e
mrNo80dy <chad.mitchell.evans@gmail.com>
2025-12-11 15:59:59 -0600

    Update page.tsx

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -419,7 +419,7 @@ export default function LearnPage() {
           </CardTitle>
         </CardHeader>
 
-        <CardContent className="space-y-4 pr-1">
+        <CardContent className="space-y-4 px-5 pr-4">
           {/* Language selectors */}
           <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
             <div className="space-y-1">
