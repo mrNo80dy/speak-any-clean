3697d11973a25f0383d3af6811d5130abb17396e
mrNo80dy <chad.mitchell.evans@gmail.com>
2026-01-16 10:19:42 -0600

    Update HudWakeZones.tsx

--- components/HudWakeZones.tsx
+++ components/HudWakeZones.tsx
@@ -10,29 +10,25 @@ type Props = {
 export function HudWakeZones({ onWakeTop, onWakeBottomRight }: Props) {
   return (
     <>
-      {/* TOP WAKE ZONE (tap only) */}
+      {/* TOP WAKE ZONE - Lower Z-Index (10) so it doesn't block buttons (30+) */}
       <div
-        className="fixed top-0 left-0 right-0 z-[60] pointer-events-auto"
+        className="fixed top-0 left-0 right-0 z-10 pointer-events-auto"
         style={{ height: "30vh", background: "transparent", touchAction: "manipulation" }}
-        onPointerDown={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
+        onPointerDown={() => {
           onWakeTop();
         }}
       />
 
-      {/* BOTTOM-RIGHT WAKE ZONE (tap only, PiP-sized) */}
+      {/* BOTTOM-RIGHT WAKE ZONE */}
       <div
-        className="fixed right-0 bottom-0 z-[60] pointer-events-auto"
+        className="fixed right-0 bottom-0 z-10 pointer-events-auto"
         style={{
           width: 150,
           height: 210,
           background: "transparent",
           touchAction: "manipulation",
         }}
-        onPointerDown={(e) => {
-          e.preventDefault();
-          e.stopPropagation();
+        onPointerDown={() => {
           onWakeBottomRight();
         }}
       />
